436657ed5b2d77491e7c1294029033cc6b3f87ed
Lab <Lab>
2016-11-28 10:39:53 +0100

    cd

--- workspace/model_prep_lqr.m
+++ workspace/model_prep_lqr.m
@@ -1,9 +1,10 @@
 model_prep
 
-x_0 = [0 0 0.1 0];
+l0 = [0 0 0 0];
+x0 = [0 0 0.1 0];
 
-Q = [1 0 0 0; 0 1 0 0; 0 0 10 0; 0 0 0 1];
-R = 1;
+Q = [1 0 0 0; 0 1 0 0; 0 0 1000 0; 0 0 0 1];
+R = 1000000;
 
-ss = linearize('model_ver_model', 'model_ver_model/Cart-Pendulum System');
-K = lqr(ss, Q, R);
+ss = linearize('model_lin', 'model_lin/Cart-Pendulum System');
+[K, S, E] = lqr(ss, Q, R);
