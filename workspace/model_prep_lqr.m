996a3baa134c9993c7b2a3c0a951e00c87545b9f
Lab <Lab>
2016-12-05 10:46:00 +0100

    cd

--- workspace/model_prep_lqr.m
+++ workspace/model_prep_lqr.m
@@ -1,10 +1,15 @@
 model_prep
 
+Tv = 0.02;
+
 l0 = [0 0 0 0];
 x0 = [0 0 0.1 0];
 
-Q = [1 0 0 0; 0 1 0 0; 0 0 1000 0; 0 0 0 1];
-R = 1000000;
+Q = [0.1 0 0 0; 0 0.001 0 0; 0 0 1 0; 0 0 0 1];
+R = 0.00001;
 
 ss = linearize('model_lin', 'model_lin/Cart-Pendulum System');
-[K, S, E] = lqr(ss, Q, R);
+[K, S, E] = lqr(ss, Q, R)
+
+%K = K.*[1 1 2 0.5];
+%K
